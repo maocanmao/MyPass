3c52d8ffff524d9e775ef46e6d55e130e2be54c5
zhouyang <yang.zhou@philips.com>
2018-06-15 14:37:32 +0800

    minor fix

--- app/src/main/java/com/maocanmao/mypass/model/entity/Account.kt
+++ app/src/main/java/com/maocanmao/mypass/model/entity/Account.kt
@@ -3,4 +3,4 @@ package com.maocanmao.mypass.model.entity
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
-data class Account(@PrimaryKey val id: String, val accountName: String, val password: String, val title: String) : RealmObject()
+open class Account(@PrimaryKey var id: Long?=null, var accountName: String?="", var password: String?="", var title: String?="") : RealmObject()
